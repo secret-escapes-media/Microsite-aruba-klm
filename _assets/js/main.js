9d5f66f779bea7a3ca63ba96445ec3c596186cd4
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-11-12 15:42:48 +0000

    rough site

---
+++ _assets/js/main.js
@@ -0,0 +1,37 @@
+// general js for the project that wouldn't be a reuseable component
+
+
+
+// ///////////////////////////////////////
+// //        Random Title color
+// ///////////////////////////////////////
+// // goes through every ".title" on the page and assigns a class to change the color
+//
+// var classNames = ['title--green','title--pink','title--blue'];
+// var classCounter = 0;
+//
+// $('.js-title-color').each(function(i) {
+//   if (classCounter > (classNames.length - 1)) {
+//     classCounter = 0;
+//   }
+//   $(this).addClass(classNames[classCounter]);
+//   classCounter++;
+// });
+//
+//
+//
+// ///////////////////////////////////////
+// //        Random Button color
+// ///////////////////////////////////////
+// // goes through every ".title" on the page and assigns a class to change the color
+//
+// var buttonClassNames = ['btn--outline-green','btn--outline-pink','btn--outline-blue'];
+// var buttonClassCounter = 0;
+//
+// $('.js-btn-color').each(function(i) {
+//   if (buttonClassCounter > (buttonClassNames.length - 1)) {
+//     buttonClassCounter = 0;
+//   }
+//   $(this).addClass(buttonClassNames[buttonClassCounter]);
+//   buttonClassCounter++;
+// });
